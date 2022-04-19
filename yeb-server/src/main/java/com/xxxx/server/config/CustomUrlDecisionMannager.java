b3dd7df3b2eb6d9482b65ad7601fe4b5f30317d8
yaozeyu3 <yaozeyu3@jd.com>
2022-04-19 17:15:20 +0800

    3333

--- yeb-server/src/main/java/com/xxxx/server/config/CustomUrlDecisionMannager.java
+++ yeb-server/src/main/java/com/xxxx/server/config/CustomUrlDecisionMannager.java
@@ -1,4 +1,58 @@
 package com.xxxx.server.config;
 
-public class CustomUrlDecisionMannager {
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * @program: yeb
+ * @description: 权限判断
+ * @packagename: com.xxxx.server.config
+ * @author: 姚泽宇
+ * @date: 2022-04-19 17:07
+ **/
+@Component
+public class CustomUrlDecisionMannager implements AccessDecisionManager {
+
+    @Override
+    public void decide(Authentication authentication, Object o, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+        for (ConfigAttribute configAttribute : configAttributes) {
+            String needRole = configAttribute.getAttribute();
+            if ("ROLE_LOGIN".equals(needRole)) {
+                if (authentication instanceof AnonymousAuthenticationToken) {
+                    throw new AccessDeniedException("尚未登陆,请登录 !");
+                } else {
+                    return;
+                }
+            }
+            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+            for (GrantedAuthority authority : authorities) {
+                if (authority.getAuthority().equals(needRole)) {
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean supports(ConfigAttribute configAttribute) {
+        return false;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
 }
