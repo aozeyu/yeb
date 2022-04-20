9447fc6e9254089843a6b197ef0abf101fa738a4
yaozeyu3 <yaozeyu3@jd.com>
2022-04-20 15:18:03 +0800

    666

--- yeb-server/src/main/java/com/xxxx/server/controller/RoleController.java
+++ yeb-server/src/main/java/com/xxxx/server/controller/RoleController.java
@@ -1,9 +1,12 @@
 package com.xxxx.server.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.xxxx.server.pojo.Menu;
+import com.xxxx.server.pojo.MenuRole;
 import com.xxxx.server.pojo.RespBean;
 import com.xxxx.server.pojo.Role;
+import com.xxxx.server.service.IMenuRoleService;
 import com.xxxx.server.service.IMenuService;
 import com.xxxx.server.service.IRoleService;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -27,6 +31,9 @@ public class RoleController {
     private IRoleService roleService;
     @Autowired
     private IMenuService menuService;
+    @Autowired
+    private IMenuRoleService menuRoleService;
+
     @ApiOperation(value = "获取所有角色")
     @GetMapping("/")
     public List<Role> getRoleList() {
@@ -34,8 +41,8 @@ public class RoleController {
     }
 
     @ApiOperation(value = "添加角色")
-    @PostMapping("/")
-    public RespBean addRole(Role role) {
+    @PostMapping("/role")
+    public RespBean addRole(@RequestBody Role role) {
         if (!role.getName().startsWith("ROLE_")) {
             role.setName("ROLE_" + role.getName());
         }
@@ -59,7 +66,18 @@ public class RoleController {
 
     @ApiOperation(value = "查询所有菜单")
     @GetMapping("/menus")
-    public List<Menu> getAllMenus () {
+    public List<Menu> getAllMenus() {
         return menuService.getAllMenus();
+    }
+
+    @ApiOperation(value = "根据角色id查询菜单id")
+    @GetMapping("/mid/{rid}")
+    public List<Integer> getMidByRid(@PathVariable Integer rid) {
+        return menuRoleService.list(new QueryWrapper<MenuRole>().eq("rid", rid)).stream().map(MenuRole::getMid).collect(Collectors.toList());
+    }
+    @ApiOperation(value = "更新角色菜单")
+    @PutMapping("/")
+    public RespBean updateMenuRole(Integer rid,Integer[] mids) {
+        return menuRoleService.updateMenuRole(rid, mids);
     }
 }
--- yeb-server/src/main/java/com/xxxx/server/service/impl/MenuRoleServiceImpl.java
+++ yeb-server/src/main/java/com/xxxx/server/service/impl/MenuRoleServiceImpl.java
@@ -1,14 +1,18 @@
 package com.xxxx.server.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xxxx.server.mapper.MenuRoleMapper;
 import com.xxxx.server.pojo.MenuRole;
+import com.xxxx.server.pojo.RespBean;
 import com.xxxx.server.service.IMenuRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author zhoubin
@@ -16,5 +20,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MenuRoleServiceImpl extends ServiceImpl<MenuRoleMapper, MenuRole> implements IMenuRoleService {
+    @Autowired
+    private MenuRoleMapper menuRoleMapper;
 
+    @Override
+    @Transactional
+    public RespBean updateMenuRole(Integer rid, Integer[] mids) {
+        menuRoleMapper.delete(new QueryWrapper<MenuRole>().eq("rid", rid));
+        if (mids == null|| 0 ==mids.length) {
+            return RespBean.success("更新成功! ");
+        }
+        Integer result = menuRoleMapper.insertRecord(rid,mids);
+        if (result == mids.length) {
+            return RespBean.success("更新成功 !");
+        }else {
+            return RespBean.error("更新失败! ");
+        }
+
+    }
 }
