40c09371c0464140e2fd9283cc712fdac9d781cf
yaozeyu3 <yaozeyu3@jd.com>
2022-04-21 10:22:36 +0800

    更新文件

--- yeb-server/src/main/java/com/xxxx/server/controller/AdminController.java
+++ yeb-server/src/main/java/com/xxxx/server/controller/AdminController.java
@@ -5,6 +5,7 @@ import com.xxxx.server.pojo.Admin;
 import com.xxxx.server.service.IAdminService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,7 @@ public class AdminController {
     @Autowired
     private IAdminService adminService;
     @ApiOperation(value = "获取所有操作员")
+    @GetMapping("/")
     public List<Admin> getAllAdmins(String keywords) {
         return adminService.getAllAdmins(keywords);
     }
